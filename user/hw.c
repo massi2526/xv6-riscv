16d5cff1bad4bfb565e4959516eaa237e8e1e8d5
massi2526 <91784799+massi2526@users.noreply.github.com>
2021-10-02 13:39:18 +0100

    Update hw.c

--- user/hw.c
+++ user/hw.c
@@ -3,7 +3,5 @@
 
 int main(int argc, char *argv[]) {
         // insert  code below to print 'Hello world' to the console
-        // you can use the printf function as you would normally.
-        // Terminate the process
-        exit(0);
+        printf("Hello world");
 }
