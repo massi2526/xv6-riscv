b04b9ddf030d047912b55f9d69c55db8c15b14be
massi2526 <91784799+massi2526@users.noreply.github.com>
2021-10-02 14:28:19 +0100

    Create hw.c

---
+++ user/hw.c
@@ -0,0 +1,9 @@
+#include "kernel/types.h"
+#include "user/user.h"
+
+int main(int argc, char *argv[]) {
+        // insert  code below to print 'Hello world' to the console
+        printf("Hello world");
+        // Terminate the process
+        exit(0);
+}
